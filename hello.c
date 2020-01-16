d31b2b6351c3c261d55a1315c713920eac7b9bbc
tstoeger2 <46796475+tstoeger2@users.noreply.github.com>
2020-01-16 08:08:48 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* "Hello World, My first program
+Author: Tyler Stoeger
+Date: 2020/01/14
+This program prints my name
+*/
+
+#include <stdio.h>
+
+int main(void) {
+printf("Tyler Stoeger\n");
+return 0;
+}
